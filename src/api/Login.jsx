3e4964372ec8e2f9d6103f4fd8027d15cc000e37
hoanglongle77 <100249743+hoanglongle77@users.noreply.github.com>
2023-12-14 10:54:58 +0700

    modify login

--- src/api/Login.jsx
+++ src/api/Login.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h1>Login</h1>
+      <h2>Label: Name</h2>
+    </div>
+  );
 };
 
 export default Login;

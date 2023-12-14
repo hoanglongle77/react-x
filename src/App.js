21fbfc1a74eeba30a10e4682ad9e05dc34d3218a
hoanglongle77 <100249743+hoanglongle77@users.noreply.github.com>
2023-12-14 11:22:26 +0700

    update in app.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -22,4 +20,3 @@ function App() {
   );
 }
 
-export default App;
